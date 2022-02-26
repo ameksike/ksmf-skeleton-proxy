892ad68c254ecf028a4708b44d74e964db987ae8
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-02-26 17:54:16 -0300

    upload project

---
+++ README.md
@@ -0,0 +1,24 @@
+origin
+	agent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'
+	host:'::1'
+	port:53999
+	token:'Basic dG9ueTphYmNkLTEyMzQ='
+	user:{
+		username: 'tony', 
+		name: 'Mr. Tony'
+	}
+
+destination
+	auth:null
+	hash:null
+	host:'www.youtube.com:443'
+	hostname:'www.youtube.com'
+	href:'//www.youtube.com:443'
+	path:null
+	pathname:null
+	port:'443'
+	protocol:null
+	query:null
+	search:null
+	slashes:true
+	url:'www.youtube.com:443'
---
+++ server/src/app/index.js
@@ -0,0 +1,31 @@
+/*
+ * @author		Antonio Membrides Espinosa
+ * @email		tonykssa@gmail.com
+ * @date		20/08/2021
+ * @copyright  	Copyright (c) 2020-2030
+ * @license    	GPL
+ * @version    	1.0
+ * */
+const KsMf = require('ksmf');
+const path = require('path');
+
+class AppModule extends KsMf.app.Module {
+    async initApp() {
+        const app = this.helper.get('app').web;
+        //... add support for ForestAdmin, Angular and React Js Router options 
+        app.get(/\/((?!(api|forest)).)*/, (req, res) => {
+            const index = path.join(__dirname, '../../' + this.opt.srv.public, 'index.html');
+            try {
+                if (fs.existsSync(path)) {
+                    res.sendFile(index);
+                } else {
+                    res.end('API v1.0.0');
+                }
+            } catch (err) {
+                console.error(err)
+                res.end('API v1.0.0');
+            }
+        });
+    }
+}
+module.exports = AppModule;
